61c5121273e1d06d85716144848b67c34757d099
Evgeniy Smirnov <zonneri@mail.ru>
2017-07-06 15:19:50 +0300

    update 3

--- src/main/java/com/itlab2017/ApiController.java
+++ src/main/java/com/itlab2017/ApiController.java
@@ -1,6 +1,8 @@
 package com.itlab2017;
 import java.util.ArrayList;
 
+import com.itlab2017.Business.Device;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,18 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    @RequestMapping("/getdevicelist")
-    public ArrayList<Device> getdevicelist(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    @RequestMapping("/devices")
+    @PostMapping
+    public ArrayList<Device> devices(@RequestParam(value="name", required=false, defaultValue="World") String name) {
         ArrayList<Device> list = new ArrayList<>();
         for (int i = 0; i<10; i++){
             Device device = new Device();
             device.id = i;
             device.name = "device " + String.valueOf(i);
             list.add(device);
-
         }
         return list;
     }
+    @PostMapping
     @RequestMapping("/test")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
 
