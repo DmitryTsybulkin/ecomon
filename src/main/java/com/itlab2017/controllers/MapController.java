9dbd992f6e961aa12bcf7dabaf054de6caa08abf
Evgeniy Smirnov <zonneri@mail.ru>
2017-07-24 15:27:22 +0300

    changed map controller

--- src/main/java/com/itlab2017/controllers/MapController.java
+++ src/main/java/com/itlab2017/controllers/MapController.java
@@ -1,5 +1,7 @@
 package com.itlab2017.controllers;
 
+import com.itlab2017.services.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MapController {
 
+    @Autowired
+    private StationService stationService;
+
+
     @RequestMapping(value = "/map", method = RequestMethod.GET)
     public String list(Model model) {
+        model.addAttribute("stations", stationService.listAllStations());
         return "map";
     }
 
