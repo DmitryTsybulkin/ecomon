e5e6e4c30b6bf5c3dc07909e19b8f7dab6f461bb
Evgeniy Smirnov <zonneri@mail.ru>
2017-07-28 14:30:51 +0300

    Merge remote-tracking branch 'remotes/origin/master-frontend' into master-server
    # Conflicts:
    #	src/main/java/com/itlab2017/bootstrap/Initializer.java
    #	src/main/java/com/itlab2017/configuration/MQTTConfiguration.java
    #	src/main/java/com/itlab2017/configuration/WebSocketConfig.java
    #	src/main/java/com/itlab2017/controllers/StationController.java
    #	src/main/java/com/itlab2017/controllers/WebApiController.java
    #	src/main/java/com/itlab2017/domain/Log.java
    #	src/main/java/com/itlab2017/domain/Sensor.java
    #	src/main/java/com/itlab2017/domain/Station.java
    #	src/main/java/com/itlab2017/handlers/MqttCallbackHandler.java
    #	src/main/java/com/itlab2017/handlers/StationWebSocketHandler.java
    #	src/main/java/com/itlab2017/mappedjson/ApiGetId.java
    #	src/main/java/com/itlab2017/services/LogServiceImpl.java
    #	src/main/resources/application.properties
    #	src/main/resources/templates/index.html
    #	src/main/resources/templates/map.html
    #	src/main/resources/templates/stations.html
    #	src/main/resources/templates/stationshow.html

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -1,6 +1,6 @@
 /*<![CDATA[*/
 function scroll() {
-    $("#logo_index").attr("src","/images/Design/Group.png");
+    $("#logo_index").attr("src", "/images/Design/Group.png");
     $(this).attr("src", "/images/Design/Group.png");
     $(".foot").show()
 }
@@ -12,15 +12,15 @@ $(".scroll-down").mouseout(function () {
     $(".scroll-down").css({"border": "2px solid white"});
     $(".scroll-down:before").css({"border": "2px solid white"});
 });
-$(document).ready(function(){
+$(document).ready(function () {
     $("#after_scroll").hide();
     $(".foot").hide();
-    $(".scroll-down").click(function(){
+    $(".scroll-down").click(function () {
         $("#panel").css({"min-height": "47px"});
         $("#after_scroll").show();
         $(".scroll-down").hide();
         $("#logo_index").animate({height: "57", width: "47"}, 500);
-        setTimeout(scroll,500);
+        setTimeout(scroll, 500);
     });
 });
 /*]]>*/
